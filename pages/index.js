ea1d532bff6d101befe2171c550712edf3f51c8f
th3-rocha <murilorocha537@gmail.com>
2022-11-14 22:17:39 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
   const estilosDaHomePage = { };
-  const mensagem = "testoesto";
 
 
   return (
@@ -16,6 +15,7 @@ function HomePage() {
         <Menu />
         <Header />
         <Timeline playlists={config.playlists}/>
+        <Favorites fav={config.fav}/>
       </div>
     </>
     
@@ -33,20 +33,44 @@ const StyledHeader = styled.div`
       height: 80px;
       border-radius:50%;
     }
+    .banner{
+      border-radius:0%;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+    }
     .user-info {
       margin-top: 5%;
       display: flex;
-
+      align-items: center;
+      gap: 10px;
     }
 
   `;
+
+const StyledFavs = styled.div`
+
+margin-top: 1%;
+display: flex;
+align-items: center;
+section{
+  display: flex;
+  align-items: center;
+  font-weight:400;
+
+}
+img{
+  width: 100px;
+  height: 100px;
+  border-radius:50%;
+}
+`;
 
 function Header() {
   "asdfasd ${eggCount}"
   return (
     <StyledHeader>
-      {/*<img src="banner"/>*/}
-
+      <img className="banner" src={`${config.banner}`}/>
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
         <div>
@@ -63,12 +87,11 @@ function Header() {
 }
 function Timeline(props) {
   const playlistNames = Object.keys(props.playlists);
-
+  
   return (
     <StyledTimeline>
       {playlistNames.map((playlistNames) => {
         const videos = props.playlists[playlistNames];
-        console.log(videos);
         return (
           <section>
             <h2>{playlistNames}</h2>
@@ -87,6 +110,33 @@ function Timeline(props) {
           </section>
         )
       })}
+    </StyledTimeline>
+  )
+}
+
+function Favorites(props){
+ 
+  console.log(props.fav);
+  return (
+    <StyledTimeline>
+      
+      <section>
+        <h2>GitTube Favorites</h2>
+        <StyledFavs>
+          {props.fav.map((favs) => {
+            return (
+              <a href={`https://github.com/${favs.github}`}>
+                <a>
+                    <img src={`https://github.com/${favs.github}.png`} />
+                  <h4>
+                    @{favs.github}
+                  </h4>
+                </a>
+              </a>
+            )
+          })}
+        </StyledFavs>
+      </section>
     </StyledTimeline>
   )
 }
